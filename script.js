851e39df7606cce752b593e96d2948ed252be167
Heitor Chans <heitorchans3@gmail.com>
2025-06-05 23:52:34 -0300

    Adição da função postar (versão beta)

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ import menuMobile from "./module/menuMobile.js";
 import selectTerrario from "./module/selectTerrario.js";
 import mostrarCriar from "./module/mostrarCriar.js";
 import posicionarBt from "./module/posicionarBt.js";
+import postar from "./module/postar.js";
 
 menuMobile();
 selectTerrario();
 mostrarCriar();
 posicionarBt();
+postar();
